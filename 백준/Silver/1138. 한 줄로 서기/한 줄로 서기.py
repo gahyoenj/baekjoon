72abef8471b12d57bbae27e41c6b57596ec18db2
gahyoenj <156265385+gahyoenj@users.noreply.github.com>
2024-03-12 17:30:20 +0900

    [Silver II] Title: 한 줄로 서기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -0,0 +1,14 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+ans = [0] * N
+
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        if cnt == arr[i] and ans[j] == 0:
+            ans[j] = i + 1
+            break
+        elif ans[j] ==0:
+            cnt += 1
+print(*ans)
