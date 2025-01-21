1e010e18cf87b6d842db9ddbd9d1640e7a4e5074
전가현 <156265385+gahyoenj@users.noreply.github.com>
2025-01-22 00:13:15 +0900

    [level 2] Title: 더 맵게, Time: 1659.85 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,13 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        mix = first + (second * 2)
+        heapq.heappush(scoville,mix)
+        answer += 1
+    return answer
