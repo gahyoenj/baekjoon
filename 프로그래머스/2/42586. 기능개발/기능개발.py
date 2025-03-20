486efbdd487aae0af5630b5774c63fda8e5fc347
전가현 <156265385+gahyoenj@users.noreply.github.com>
2025-03-20 13:05:35 +0900

    [level 2] Title: 기능개발, Time: 0.63 ms, Memory: 9.12 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,16 @@
+def solution(progresses, speeds):
+    answer = []
+    while progresses:
+        for idx in range(len(progresses)):
+            progresses[idx] += speeds[idx]
+        
+        cnt = 0
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+        
+        if cnt > 0:
+            answer.append(cnt)
+        # print(progresses)
+    return answer
