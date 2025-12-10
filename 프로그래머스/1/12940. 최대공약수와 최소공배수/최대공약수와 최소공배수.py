6e0eddc4c0619a8c0c41d4cf48afa1d45a33d603
전가현 <156265385+gahyoenj@users.noreply.github.com>
2025-12-10 10:20:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 50.67 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,15 @@
+def solution(n, m):
+    answer = []
+    if n > m:
+        n,m= m,n
+    for i in range(m,0,-1):
+        if n % i == 0 and m %i == 0:
+            answer.append(i)
+            break
+    
+    for i in range(m,n*m+1):
+        if i % n == 0 and i % m == 0:
+            answer.append(i)
+            break
+    
+    return answer
